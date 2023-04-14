248179c5504b3f28f563a7c08510058466adddac
Elvina Fattakhova <elvinaf1996@gmail.com>
2023-04-14 14:45:28 +0300

    second

--- src/test/java/BaseRunner.java
+++ src/test/java/BaseRunner.java
@@ -1,9 +1,7 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SimpleReport;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,30 +10,21 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/resources/features",
-        glue = "step_definitions",
-        dryRun = false,
-        plugin = {"html:target/cucumber-report/smoketest", "json:target/cucumber.json"},
-        snippets = CucumberOptions.SnippetType.UNDERSCORE)
+        glue = "step_definitions")
 public abstract class BaseRunner {
 
-    private final SimpleReport report = new SimpleReport();
-
     @Before
-    public void setUp(Scenario scenario) {
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.driverManagerEnabled = true;
         Configuration.headless = false;
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 5000L;
-        scenario.log("Starting " + scenario.getName());
-        report.start();
     }
 
     @After
-    public void turnDown(Scenario scenario) {
+    public void turnDown() {
         Selenide.closeWebDriver();
-        scenario.log("Finished " + scenario.getName());
-        report.finish(scenario.getName());
     }
 }
